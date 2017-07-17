015df908df7ab450691b453c7050476faadd3c77
Alberto Vega Gonzalez <alberto@Albertos-MacBook-Pro.local>
2017-07-16 21:23:50 -0700

    Added Design Patterns Playground

--- DesignPatterns.playground/Contents.swift
+++ DesignPatterns.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/*:
+#Design Patterns Problems
+
+
+ 1. Your application uses a logging class to write debugging messages to the console. How would you implement this logging facility using the Singleton pattern?
+
+ Follow up:
+ 2. Your application uses a singleton, but it's not always necessary, and it's expensive to initialize. How can you improve this situation?
+ 
+ The solution is using deferred initialization also called lazy initialization or lazy loading.
+ 
+ 
+ */
+
+class Logger {
+    // The lazy initializer for the static variable (also for static members of structs and enums) is run the first time that global is accessed, and is launched as `dispatch_once` to make sure that the initialization is atomic.
+    // source: https://krakendev.io/blog/the-right-way-to-write-a-singleton
+    static let sharedInstance = Logger()
+    private init() {} //This prevents others from using the default '()' initializer for this class.
+    
+    func log(message: String) {
+        print("\(Date()) \(message)")
+    }
+}
+
+//: Testing code
+
+//Logger.sharedInstance.log(message: "this is a test")
