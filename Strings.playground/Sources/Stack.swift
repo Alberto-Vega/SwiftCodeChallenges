5ec0535b31b6b9ee129e344b0a573799c2b0ca1b
Alberto Vega Gonzalez <alberto@Albertos-MacBook-Pro.local>
2017-07-16 20:38:29 -0700

    Added Temperature tracker

---
+++ Strings.playground/Sources/Stack.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public struct Stack<T> {
+    var items = [T]()
+    public mutating func push(item: T) {
+        items.append(item)
+    }
+    public mutating func pop() -> T {
+        return items.removeLast()
+    }
+    
+    public var isEmpty: Bool {
+        return items.isEmpty
+    }
+    public init(items:[T]) {
+            self.items = items
+    }
+}
